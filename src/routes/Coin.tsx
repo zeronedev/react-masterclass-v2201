d87f924d71b93715b47cd088c95f7267e86646f0
zeronedev <ok7375@gmail.com>
2022-01-29 15:59:29 +0900

    #4.8 Nested Routes part Two : ver6 useMatch()

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link, useMatch } from "react-router-dom";
 import { Route, Routes, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 import Chart from "./Chart";
@@ -52,6 +53,27 @@ const Description = styled.p`
   margin: 20px 0px;
 `;
 
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`;
 interface RouteState {
   state: {
     name: string;
@@ -119,6 +141,8 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch(`/${coinId}/chart`);
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -168,6 +192,14 @@ function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
           <Routes>
             <Route path={`price`} element={<Price />}></Route>
             <Route path={`chart`} element={<Chart />}></Route>
