8c0287561dea5f138701a42034c1779783f7381a
zeronedev <ok7375@gmail.com>
2022-01-30 20:23:11 +0900

    #4.13 Price Chart part Two: 데이타 시각화 apexcharts

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,68 @@
-function Chart() {
-  return <h1>Chart</h1>;
+import ApexChart from "react-apexcharts";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -216,7 +216,7 @@ function Coin() {
           </Tabs>
           <Routes>
             <Route path={`price`} element={<Price />}></Route>
-            <Route path={`chart`} element={<Chart />}></Route>
+            <Route path={`chart`} element={<Chart coinId={coinId!} />}></Route>
           </Routes>
         </>
       )}
